9811986365cd0a75172effe53a036d9deb379b54
Your Name <you@example.com>
2024-12-14 02:40:18 +0100

    EXERCICE 4

--- tests/Feature/ChirpTest.php
+++ tests/Feature/ChirpTest.php
@@ -9,7 +9,6 @@ use Tests\TestCase;
 
 class ChirpTest extends TestCase
 {
-    use RefreshDatabase;
 
     /**
      * A basic feature test example.
@@ -66,4 +65,20 @@ class ChirpTest extends TestCase
             $reponse->assertSee($chirp->message);
         }
     }
+    public function test_un_utilisateur_peut_modifier_son_chirp()
+{
+ $utilisateur = User::factory()->create();
+ $chirp = Chirp::factory()->create(['user_id' => $utilisateur->id]);
+ $this->actingAs($utilisateur);
+ $reponse = $this->put("/chirps/{$chirp->id}", [
+ 'message' => 'Chirp modifié'
+ ]);
+ $reponse->assertStatus(200);
+ // Vérifie si le chirp existe dans la base de donnée.
+ $this->assertDatabaseHas('chirps', [
+ 'id' => $chirp->id,
+ 'message' => 'Chirp modifié',
+ ]);
+}
+
 }
